91821d9ae02b3bba85b105c968546bd629155cac
DevisreePutta <117871006+DevisreePutta@users.noreply.github.com>
2025-06-26 22:38:55 -0400

    Update ValueCalc.py
    Signed-off-by: DevisreePutta <117871006+DevisreePutta@users.noreply.github.com>

--- New/ValueCalc.py
+++ New/ValueCalc.py
@@ -1,17 +1,14 @@
-#idk if i gotta import anything, Angelina cld u take care of this if imports r needed?? thxxx
-
+#do i need to import anything??
 #will change when i get more info as to what were acc doing lmao, asked Taryn already as to wut EXACTLY value-per-mile MEEANS loll + what she wants for the calc
 
 
-#function is for value per mile for da FLIGHT stuff, based on monetary value per mile
-def value_per_mile_flight(cash_value, miles, taxes_fees): #cash_value and miles are COSTS
+#NOTE: FInish Friday Morning??!
+def value_per_mile(cash_value, miles, taxes_fees): #cash_value and miles are COSTS
   #change if definiton/understanding is wrong
-
   if miles == 0:
     return 0
-
   net_monetary_value = cash_value - taxes_fees
-
+  
   return round( net_monetary_value/miles, 4)  #4th place good?
 
 
@@ -20,3 +17,20 @@ def value_per_mile_flight(cash_value, miles, taxes_fees): #cash_value and miles 
 # make sure these functions fit in well to the later stuff?? (like reading the data what we gotta read n how n stuff)
 #Any way to sample test if needed??
   
+#dictionary:
+  #KEY: Acc Hotels: Marriott International, Hilton Worldwide, InterContinental Hotels Group, Wyndham Hotels & Resorts  VALUE: another dictionary
+      #KEY2: The Diff Hotel Chains in Diff MAJOR Cities, of diff countries
+          #Key2: Diff suite names 
+              #KEY3: Price: Marriot Grand suite Paris price --> NUMBER (miles??)
+              #KEY4: Upgrades + Value: Free break fast n stuff (Strings with certain format so detectable)
+
+# --> Algorithm: IF there are those extra value stuff based on the string: WHAT the upgrade is, a certain percentage increase on the value per mile 
+
+aNum = 0
+#For amenities: Either include estimated extra value or string ofspecific stuff to then be read in, and the value increased based on the og price
+
+HOTELS_INFO:
+  {"Marriott International": {"Paris Marriott Champs Elysees": {"Price": aNum, "Amenities": [] }, 
+                             "Paris Marriott Rive Gauche Hotel": {},
+                             "Paris Marriott Opera": {} },
+   "Hilton Worldwide": {} }
