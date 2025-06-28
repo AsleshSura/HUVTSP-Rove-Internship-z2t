8884061d9b219c5fa85c3d923ad3808e9b24fef3
DevisreePutta <117871006+DevisreePutta@users.noreply.github.com>
2025-06-27 22:53:40 -0400

    Update ValueCalc.py
    Signed-off-by: DevisreePutta <117871006+DevisreePutta@users.noreply.github.com>

--- New/ValueCalc.py
+++ New/ValueCalc.py
@@ -1,11 +1,8 @@
 #do i need to import anything??
-#will change when i get more info as to what were acc doing lmao, asked Taryn already as to wut EXACTLY value-per-mile MEEANS loll + what she wants for the calc
 
 
-#NOTE: FInish Friday Morning??!
 #NOTE: We were unable to find the exact number of miles earned and redeemed for gift cards. Plan to go to office hours and will update this soon.
 def value_per_mile(cash_value, miles, taxes_fees): #cash_value and miles are COSTS
-  #change if definiton/understanding is wrong
   if miles == 0:
     return 0
   net_monetary_value = (cash_value - taxes_fees)
@@ -13,24 +10,19 @@ def value_per_mile(cash_value, miles, taxes_fees): #cash_value and miles are COS
   return round( net_monetary_value/miles, 4)  #4th place good?
 
 
-#Steps: (tho i lowk dfk) --> Add more info after talkin to Taryn
-# create diff val calcs for flights n hotels n somehow some function for the girft cards
-# make sure these functions fit in well to the later stuff?? (like reading the data what we gotta read n how n stuff)
-#Any way to sample test if needed??
-  
-#dictionary:
+ 
+#dictionary format:
   #KEY: Acc Hotels: Marriott International, Hilton Worldwide, InterContinental Hotels Group, Wyndham Hotels & Resorts  VALUE: another dictionary
       #KEY2: The Diff Hotel Chains in Diff MAJOR Cities, of diff countries
           #Key2: Diff suite names 
               #KEY3: Price: Marriot Grand suite Paris price --> NUMBER (miles??)
               #KEY4: Upgrades + Value: Free break fast n stuff (Strings with certain format so detectable)
 
-# --> Algorithm: IF there are those extra value stuff based on the string: WHAT the upgrade is, a certain percentage increase on the value per mile 
 
 #Also maybe we can add more data and put it into a json file (idk how to import n do that stuff n if there are any extra cautions w dat sooo)
-aPrice = 0
-aMile = 0
-#For amenities: Either include estimated extra value or string ofspecific stuff to then be read in, and the value increased based on the og price
+
+#IMPORTANT: The "bonuses" are "extra" things that you would get if you pay with miles, and since you get more "features" we must factor this into the value per mile calculation
+
 #Price estimates in dollars
 #The room types are averages per suite type, so things like location in hotel or view are not seperately factored in, the room values are "averaged"
 HOTELS_INFO:
@@ -204,7 +196,7 @@ HOTELS_INFO:
     "Hilton Worldwide": {} #Update Later lol?? W/ more data, possibly API instead of manual [took a LONGGG time]
 }
 
-
+#Keys and Value categories
 #Bonuses: Possible Upgrade = 0.05   Late Checkout = 0.015   Balcony = 0.05    View = 0.1    Breakfast = 0.03    Nonstrict Time =  0.03 (Early check in / Late Checkout)   Lounge Access: 0.08  
 #Hotel Wide Amenities: Gym    Housekeeping   Business Center   Evening Turndown Service     Sundry/Convenience Store    Dog Friendly w/ Fee
 
