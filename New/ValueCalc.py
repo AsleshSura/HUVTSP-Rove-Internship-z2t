5a895c90483db8c61b9559dc382e836c75640b42
DevisreePutta <117871006+DevisreePutta@users.noreply.github.com>
2025-06-27 22:18:53 -0400

    Update ValueCalc.py
    Signed-off-by: DevisreePutta <117871006+DevisreePutta@users.noreply.github.com>

--- New/ValueCalc.py
+++ New/ValueCalc.py
@@ -36,44 +36,174 @@ aMile = 0
 HOTELS_INFO:
 {
     "Marriott International": 
-   {
+     {
      "Paris Marriott Champs Elysees":    
         {                         # $$    miles
           "Deluxe King": 
             {  
               "Price": [810, 31667], 
-              "Bonus_Through_Redemption": 
+              "Bonus Through Redemption": 
                 {
                   "Total": 0
                 },  
             }, 
-          "Room2": 
+          "Champs-Elysees Signature Suite": 
             {  
               "Price": [1161.5, 84500], 
-              "Bonus_Through_Redemption": 
+              "Bonus Through Redemption": 
                 {
-                  "Possible_Upgrade": 0.15, "Balcony": 0.2, "View": 0.3, "Breakfast": 0.08, "Nonstrict_Time": 0.03, "Total": 0.31,
+                  "Possible Upgrade": 0.05, "Balcony": 0.05, "View": 0.1, "Breakfast": 0.03, "Nonstrict Time": 0.03, "Total": 0.31,
                 },  
             },
-          "Room3": 
+          "Core Amenities": ["Gym", "Housekeeping", "Business Center", "Evening Turndown Service"],
+          },
+     "New York Marriott Marquis": 
+     {
+      "Deluxe (Guest room, 2 Double)": 
             {  
-              "Price": [, ], 
-              "Bonus_Through_Redemption": 
+              "Price": [484, 26000], 
+              "Bonus Through Redemption": 
                 {
-                   "Total": ,
+                  "Breakfast": 0.03, "Nonstrict Time": 0.03, "Total": 0.13
                 },  
             },
-          "Core Amenities": [],
-        },
-     "New York Marriott Marquis": {},
-     "Wailea Beach Resort - Marriott, Maui": {},
-     "The Cosmopolitan of Las Vegas": {},
-     "JW Marriott Marco Island Beach Resort": {},
-     "Sheraton Grand Seattle": {},
+       "Premier Large Guest room (1 King, Sofa bed, Corner room)": 
+            {  
+              "Price": [579, 36000], 
+              "Bonus Through Redemption": 
+                {
+                  "Breakfast": 0.03, "Nonstrict Time": 0.03, "Total": 0.13
+                },  
+            },
+       "Executive Suite (M Club lounge access, 1 Bedroom Larger Suite, 1 King, Sofa bed)": 
+            {  
+              "Price": [1218, 86000], 
+              "Bonus Through Redemption": 
+                {
+                  "Breakfast": 0.3, "Nonstrict Time": 0.03, "Lounge Access": 0.08, "Possible Upgrade": 0.05, "Total": 0.26
+                },  
+            },
+          "Core Amenities": ["Gym", "Business Center"],
+     },
+     "Wailea Beach Resort - Marriott, Maui": 
+       {    
+       "Garden View King (Guest room, 1 King, Sofa bed, Balcony)": 
+            {  
+              "Price": [658, 29334], 
+              "Bonus Through Redemption": 
+                {
+                  "Breakfast": 0.03, "Late Checkout": 0.015, "Total": 0.215
+                },  
+            },
+       "Junior Suite (1 King, Balcony)": 
+            {  
+              "Price": [849, 44667], 
+              "Bonus Through Redemption": 
+                {
+                  "Breakfast": 0.03, "Late Checkout": 0.015, "Balcony": 0.05, "Possible Upgrade": 0.05, "Total": 0.215
+                },  
+            },
+       "Ocean Suite (1 Bedroom Suite, 1 King, Sofa bed, Ocean view, Balcony)": 
+            {  
+              "Price": [1241, 76000], 
+              "Bonus Through Redemption": 
+                {
+                  "Possible Upgrade": 0.05, "Breakfast": 0.03, "Balcony": 0.05, "View": 0.1, "Late Checkout": 0.015, "Total": 0.315
+                },  
+            },
+          "Core Amenities": ["Gym", "Housekeeping", "Business Center", "Sundry/Convenience Store"],
+       },
+     "The Cosmopolitan of Las Vegas": 
+       {
+         
+       "City Room (Guest room, 2 Queen)": 
+            {  
+              "Price": [425, 24000], 
+              "Bonus Through Redemption": 
+                {
+                  "Breakfast": 0.03, "Late Checkout": 0.015, "Total": 0.045
+                },  
+            },
+       "Terrace Studio (Guest room, 1 King)": 
+            {  
+              "Price": [475, 25667], 
+              "Bonus Through Redemption": 
+                {
+                  "Possible Upgrade": 0.05, "Breakfast": 0.03, "Late Checkout": 0.015, "Total": 0.095
+                },  
+            },
+       "Terrace Suite (Guest room, 1 King, Fountain view)": 
+            {  
+              "Price": [565, 30667], 
+              "Bonus Through Redemption": 
+                {
+                  "Possible Upgrade": 0.05, "Breakfast": 0.03, "Late Checkout": 0.015, "View": 0.1, "Total": 0.195
+                },  
+            },
+          "Core Amenities": ["Dog Friendly w/ Fee", "Gym", "Housekeeping", "Business Center", "Sundry/Convenience Store"],
+       },
+     "JW Marriott Marco Island Beach Resort": {
+       
+       "Tropical View (Guest room, 2 Queen, Balcony)": 
+            {  
+              "Price": [515, 24834], 
+              "Bonus Through Redemption": 
+                {
+                  "Breakfast": 0.03, "Late Checkout": 0.015, "Balcony": 0.05, "Total": 0.095
+                },  
+            },
+       "Tropical View, Guest room, 1 King, Balcony": 
+            {  
+              "Price": [515, 24834], 
+              "Bonus Through Redemption": 
+                {
+                  "Breakfast": 0.03, "Late Checkout": 0.015, "Balcony": 0.05, "Total": 0.095
+                },  
+            },
+       "Pool & Gulf View, Guest room, 2 Queen, Balcony": 
+            {  
+              "Price": [539, 28500], 
+              "Bonus Through Redemption": 
+                {
+                  "Breakfast": 0.03, "Late Checkout": 0.015, "Total": 0.0415
+                },  
+            },
+          "Core Amenities": ["Gym", "Business Center", "Sundry/Convenience Store"],
+     },
+     "Sheraton Grand Seattle": 
+       {
+         
+       "Guest Room (2 Double)": 
+            {  
+              "Price": [252, 16000], 
+              "Bonus Through Redemption": 
+                {
+                  "Breakfast": 0.03, "Late Checkout": 0.015, "Total": 0.0415
+                },  
+            },
+       "Deluxe Guest Room (2 Double, City view, High floor)": 
+            {  
+              "Price": [274, 17667], 
+              "Bonus Through Redemption": 
+                {
+                  "Breakfast": 0.03, "Late Checkout": 0.015, "Total": 0.0415
+                },  
+            },
+       "Premium Large Guest Room (1 King)": 
+            {  
+              "Price": [297, 19334], 
+              "Bonus Through Redemption": 
+                {
+                  "Possible Upgrade": 0.05, "Breakfast": 0.03, "Late Checkout": 0.015, "Total": 0.0915
+                },  
+            },
+          "Core Amenities": ["Dog Friendly", "Gym", "Housekeeping", "Business Center", "Sundry/Convenience Store", "Evening Turndown Service"],
+       },
     },
   
-    "Hilton Worldwide": {} 
+    "Hilton Worldwide": {} #Update Later lol?? W/ more data, possibly API instead of manual [took a LONGGG time]
 }
 
-#
-#Bonuses: Possible_Upgrade = 0.15   Balcony = 0.2    View = 0.3    Breakfast = 0.08    Nonstrict_Time =  0.03 (Early check in / Late Checkout)   
+
+#Bonuses: Possible Upgrade = 0.05   Late Checkout = 0.015   Balcony = 0.05    View = 0.1    Breakfast = 0.03    Nonstrict Time =  0.03 (Early check in / Late Checkout)   Lounge Access: 0.08  
+#Hotel Wide Amenities: Gym    Housekeeping   Business Center   Evening Turndown Service     Sundry/Convenience Store    Dog Friendly w/ Fee
